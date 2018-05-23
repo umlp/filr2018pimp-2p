b70e044eb052c4d7709e5cd36d43125320a6595b
tguiraud <36045940+tguiraud@users.noreply.github.com>
2018-05-23 14:39:38 +0200

    Suppression barre menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,7 +28,7 @@
 <body id="app-layout">
 
 
-<-- barre de menu -->
+<!-- 
 <nav class="navbar navbar-inverse">
   <div class="container-fluid">
     <div class="navbar-header">
@@ -63,9 +63,11 @@
 
     @yield('content')
 
-    <!-- JavaScripts -->
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
     {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
+    
+    -->
 </body>
 </html>
