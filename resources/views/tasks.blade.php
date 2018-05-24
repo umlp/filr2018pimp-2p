7d73be49908d76ae973665aa44b81cf63f59d843
pcadorel <36007955+pcadorel@users.noreply.github.com>
2018-05-24 20:56:38 +0200

    Update tasks.blade.php

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -17,51 +17,29 @@
 
           <!-- Task Name -->
           <div class="form-group">
-	  <div class="row">
-            <label for="task-name" class="col-sm-3 control-label">Task</label>
-
-            <div class="col-sm-6">
-              <input type="text" name="name" id="task-name" class="form-control" value="{{ old('task') }}">
-            </div>
-	    </div>
-            <label for="Champ-nom" class="col-sm-3 control-label">Nom</label>
-            <div class="col-sm-6">
-              <input type="text" name="Nom" id="Champ-nom" class="form-control"/>
-            </div>
+            
+            <input type="text" name="name" id="task-name" class="form-control" value="{{ old('task') }}">
+           
           </div>
 
           <!-- Add Task Button -->
           <div class="form-group">
             <div class="col-sm-offset-3 col-sm-6">
               <button type="submit" class="btn btn-default">
-                <i class="fa fa-btn fa-plus"></i>Add Task
+                <i class="fa fa-btn fa-plus">Add Task</i>
               </button>
             </div>
           </div>
         </form>
-      </div>
-    </div>
-    <div class="gestionnaireCompte" class="panel panel-default">
-      <div class="panel-heading">
+
+
       </div>
 
-      <div class="panel-body">
-	<div class="form-group">
-	  <label for="identifiant" class="col-sm-3 control-label">Identifiant</label>
-	  <div class="col-sm-6">
-	    <input type="text" name="identifiant" id="identifiant">
-	  </div>
 
-	  <br/><br/>
-	  <label for="motDePasse" class="col-sm-3 control-label">Mot de passe</label>
-	  <div class="col-sm-6">
-	    <input type="password" name="motDePasse" id="motDePasse">
-	  </div>
-	</div>
-      </div>
     </div>
 
-    <!-- Current Tasks -->
+
+      <!-- Current Tasks -->
     @if (count($tasks) > 0)
     <div class="panel panel-default">
       <div class="panel-heading">
@@ -97,6 +75,93 @@
       </div>
     </div>
     @endif
+
+
+
+
+
+     @if (count($comptes) > 0)
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        Comptes enregistres
+      </div>
+
+      <div class="panel-body">
+        <table class="table table-striped task-table">
+          <thead>
+            <th>Compte</th>
+            <th>&nbsp;</th>
+          </thead>
+          @foreach ($comptes as $compte)
+          <tbody>
+	    <tr>
+	      <td class="table-text"><div>Mail</div></td>
+	      <td class="table-text"><div>{{ $compte->mail }}</div></td>
+
+	      <!-- Task Delete Button -->
+	      <td>
+	      </td>
+	    </tr>
+	    <tr>
+	      <form action="{{ url('compte/'.$compte->id) }}" method="POST">
+                {{ csrf_field() }}
+                {{ method_field('DELETE') }}
+		
+                <button type="submit" class="btn btn-danger">
+                  <i class="fa fa-btn fa-trash">Supprimer</i>
+                </button>
+              </form>
+	    </tr>
+          </tbody>
+
+            @endforeach
+
+        </table>
+      </div>
+    </div>
+    @endif
+
+
+
+
+
+
+    <div class="gestionnaireCompte" class="panel panel-default">
+      <div class="panel-heading">
+      </div>
+
+      <div class="panel-body">
+
+	<form action="{{ url('compte')}}" method="POST" class="form-horizontal">
+	  <div class="form-group">
+	    <div class="row">
+	      <label for="mail" class="col-sm-3 control-label">Adresse Mail</label>
+	      <div class="col-sm-6">
+		<input type="mail" name="mail" id="mail">
+	      </div>
+	    </div>
+
+	    <div class="row">
+	      <label for="identifiant" class="col-sm-3 control-label">Identifiant</label>
+	      <div class="col-sm-6">
+		<input type="text" name="identifiant" id="identifiant">
+	      </div>
+	    </div>
+
+	    <div class="row">
+	      <label for="motDePasse" class="col-sm-3 control-label">Mot de passe</label>
+	      <div class="col-sm-6">
+		<input type="password" name="motDePasse" id="motDePasse">
+	      </div>
+	    </div>
+
+	  </div>
+	</form>
+
+      </div>
+    </div>
+
+  
   </div>
 </div>
 @endsection
