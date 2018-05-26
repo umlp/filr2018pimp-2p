b50fa176a26e027f1efc6bfb25bda6fe3e778011
tguiraud <36045940+tguiraud@users.noreply.github.com>
2018-05-26 19:04:09 +0200

    Update tasks.blade.php

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -4,10 +4,10 @@
 	<div class="container">
 		<div class="row">
 			<div>
-				<div class="row" style="height:90px;">
+				<div class="row" style="height:250px;">
 					<div class="col-xs-4"> 
 						<h2> Facebook </h2>
-						<img src="https://s1.qwant.com/thumbr/0x0/8/f/79cf42cf180a109b26f887f1d41aef18b5b2e30c98dadb10dd76ee238a0055/facebook_logos_PNG19753.png?u=http%3A%2F%2Fpngimg.com%2Fuploads%2Ffacebook_logos%2Ffacebook_logos_PNG19753.png&q=0&b=1&p=0&a=1" style="width:50%" alt="application logo" />
+						<img src="https://s1.qwant.com/thumbr/0x0/8/f/79cf42cf180a109b26f887f1d41aef18b5b2e30c98dadb10dd76ee238a0055/facebook_logos_PNG19753.png?u=http%3A%2F%2Fpngimg.com%2Fuploads%2Ffacebook_logos%2Ffacebook_logos_PNG19753.png&q=0&b=1&p=0&a=1" style="height:80%" alt="application logo" />
 					</div>
 
 					<div class="col-xs-8" style="display:flex; justify-content:space-around; flex-direction:column;"> 
