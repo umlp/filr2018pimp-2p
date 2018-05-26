0dc97e9e427390f70a7dca733d7d3a3ca6309550
tguiraud <36045940+tguiraud@users.noreply.github.com>
2018-05-26 18:57:18 +0200

    Update tasks.blade.php

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -10,7 +10,7 @@
 						<img src="https://s1.qwant.com/thumbr/0x0/8/f/79cf42cf180a109b26f887f1d41aef18b5b2e30c98dadb10dd76ee238a0055/facebook_logos_PNG19753.png?u=http%3A%2F%2Fpngimg.com%2Fuploads%2Ffacebook_logos%2Ffacebook_logos_PNG19753.png&q=0&b=1&p=0&a=1" style="width:50%" alt="application logo" />
 					</div>
 
-					<div class="col-xs-8" style="display:flex; justify-content:space-around;"> 
+					<div class="col-xs-8" style="display:flex; justify-content:space-around; flex-direction:column;"> 
 						<div style="flex:1;">
 							<div class="col-xs-5"> Identifiant : </div>
 							<div class="col-xs-7"> unmail@exemple.com </div>
@@ -34,7 +34,7 @@
 						<a href="#"> modifier <a/>
 					</div>
 					
-					<div class="col-xs-3> </div>
+					<div class="col-xs-3"> </div>
 				</div>
 			</div>
 		</div>
