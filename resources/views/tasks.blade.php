c8474487c49a052fb06ba1b9fb808bdb31867601
tguiraud <36045940+tguiraud@users.noreply.github.com>
2018-05-26 18:55:23 +0200

    avec flex

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -5,17 +5,17 @@
 		<div class="row">
 			<div>
 				<div class="row">
-					<div class="col-xs-4" style="background-color:grey;"> 
+					<div class="col-xs-4"> 
 						<h2> Facebook </h2>
 						<img src="https://s1.qwant.com/thumbr/0x0/8/f/79cf42cf180a109b26f887f1d41aef18b5b2e30c98dadb10dd76ee238a0055/facebook_logos_PNG19753.png?u=http%3A%2F%2Fpngimg.com%2Fuploads%2Ffacebook_logos%2Ffacebook_logos_PNG19753.png&q=0&b=1&p=0&a=1" style="width:50%" alt="application logo" />
 					</div>
 
-					<div class="col-xs-8" style="background-color:grey;"> 
-						<div class="row">
+					<div class="col-xs-8" style="display:flex; justify-content:space-around;"> 
+						<div style="flex:1;">
 							<div class="col-xs-5"> Identifiant : </div>
 							<div class="col-xs-7"> unmail@exemple.com </div>
 						</div>
-						<div class="row">
+						<div style="flex:1;>
 							<div class="col-xs-5"> Mot de Passe </div>
 							<div class="col-xs-7"> ********* </div>
 						</div>
@@ -23,7 +23,7 @@
 				</div>
 				
 				<div class="row">
-					<div class="col-xs-3> </div>
+					<div class="col-xs-3"> </div>
 					
 					<div class="col-xs-3"> 
 						<a href="#"> Supprimer <a/>
