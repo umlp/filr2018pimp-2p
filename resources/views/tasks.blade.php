0afe7138638bf7dbbc125f003cf9e0295720de68
tguiraud <36045940+tguiraud@users.noreply.github.com>
2018-05-26 19:01:51 +0200

    Update tasks.blade.php

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -4,7 +4,7 @@
 	<div class="container">
 		<div class="row">
 			<div>
-				<div class="row">
+				<div class="row" style="height:90px;">
 					<div class="col-xs-4"> 
 						<h2> Facebook </h2>
 						<img src="https://s1.qwant.com/thumbr/0x0/8/f/79cf42cf180a109b26f887f1d41aef18b5b2e30c98dadb10dd76ee238a0055/facebook_logos_PNG19753.png?u=http%3A%2F%2Fpngimg.com%2Fuploads%2Ffacebook_logos%2Ffacebook_logos_PNG19753.png&q=0&b=1&p=0&a=1" style="width:50%" alt="application logo" />
@@ -31,7 +31,7 @@
 					
 					
 					<div class="col-xs-3"> 
-						<a href="#"> modifier <a/>
+						<a href="#"> modifier </a>
 					</div>
 					
 					<div class="col-xs-3"> </div>
