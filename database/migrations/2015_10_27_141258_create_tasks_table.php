934b68c5696886bd2cf4dd041feb524015d006c7
pcadorel <36007955+pcadorel@users.noreply.github.com>
2018-06-05 14:49:53 +0200

    Update 2015_10_27_141258_create_tasks_table.php

--- database/migrations/2015_10_27_141258_create_tasks_table.php
+++ database/migrations/2015_10_27_141258_create_tasks_table.php
@@ -14,9 +14,7 @@ class CreateTasksTable extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('url');
-            $table->string('name');
-            $table->string('password');
+            $table->string('name');            
             $table->timestamps();
         });
     }
