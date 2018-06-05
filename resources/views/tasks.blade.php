4228c8130b6590d8723f772645e2a40388b66b84
pcadorel <36007955+pcadorel@users.noreply.github.com>
2018-06-05 16:13:00 +0200

    Update tasks.blade.php

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -1,78 +1,216 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="col-sm-offset-2 col-sm-8">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    New Task
-                </div>
 
-                <div class="panel-body">
-                    <!-- Display Validation Errors -->
-                    @include('common.errors')
+<style>
+  .row {
+  
+  }
+ 
+  .blockHeader {
+  color : white;
+  background-color: grey;
+  display: flex;
+  flex-direction : row;
+  justify-content: space-between;
+  padding: 10px;
+  padding-left: 20px;
+  padding-right: 20px;
+  border-radius: 10px 10px 0px 0px;
+  }
+  
+  .information {
+  display: flex;
+  flex-direction: row;
+  min-width:350px;
+  }
+  
+  .dataLabel {
+  color: black;
+  display: inline;
+  font-weight: bold;
+  padding-left: 20px;
+  min-width:150px;
+  }
+  
+  .data {
+  display: block;
+  }
+  
+  .logo {
+  margin: 10px;
+  width : 50%;
+  }
+  
+  .subcontainer {
+  margin-top : 10px;
+  margin-bottom: 10px;
+  border-radius: 14px 14px 10px 10px;
+  border: 2px solid grey;
+  min-width: 750px;
+  justify-content : space-around;
+  align-items : center;
+  }
+  
+  .applicationTitle {
+  font-weight: bold;
+  font-size: 16px;
+  }
+  
+  .colonne {
+  height:100%; 
+  display:flex; 
+  flex-direction:column; 
+  justify-content:space-around;
+  flex-wrap : no wrap;
+  }
+  
+  .input {
+  align-self: center;
+  }
+</style>
 
-                    <!-- New Task Form -->
-                    <form action="{{ url('task')}}" method="POST" class="form-horizontal">
-                        {{ csrf_field() }}
-
-                        <!-- Task Name -->
-                        <div class="form-group">
-                            <label for="task-name" class="col-sm-3 control-label">Task</label>
-
-                            <div class="col-sm-6">
-                                <input type="text" name="name" id="task-name" class="form-control" value="{{ old('task') }}">
-                            </div>
-                        </div>
-
-                        <!-- Add Task Button -->
-                        <div class="form-group">
-                            <div class="col-sm-offset-3 col-sm-6">
-                                <button type="submit" class="btn btn-default">
-                                    <i class="fa fa-btn fa-plus"></i>Add Task
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-
-            <!-- Current Tasks -->
-            @if (count($tasks) > 0)
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        Current Tasks
-                    </div>
-
-                    <div class="panel-body">
-                        <table class="table table-striped task-table">
-                            <thead>
-                                <th>Task</th>
-                                <th>&nbsp;</th>
-                            </thead>
-                            <tbody>
-                                @foreach ($tasks as $task)
-                                    <tr>
-                                        <td class="table-text"><div>{{ $task->name }}</div></td>
-
-                                        <!-- Task Delete Button -->
-                                        <td>
-                                            <form action="{{ url('task/'.$task->id) }}" method="POST">
-                                                {{ csrf_field() }}
-                                                {{ method_field('DELETE') }}
-
-                                                <button type="submit" class="btn btn-danger">
-                                                    <i class="fa fa-btn fa-trash"></i>Delete
-                                                </button>
-                                            </form>
-                                        </td>
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            @endif
-        </div>
+<div class="container">
+  <div class="row subcontainer">
+    <div class="blockHeader">
+      <span class="applicationTitle"> Facebook </span>
+      <a href="#" style="color:white;"> supprimer </a>
     </div>
+    
+    <div class="information">
+      <div class="col-xs-3" style="text-align:center;"> 
+	<img src="https://upload.wikimedia.org/wikipedia/fr/thumb/c/c8/Twitter_Bird.svg/1259px-Twitter_Bird.svg.png" alt="Logo Facebook" class="logo" />
+      </div>
+      
+      <div class="col-xs-9" style="display:flex; flex-direction:column; justify-content:space-around;">
+	<div class="row" style="flex:3;">
+	  <div class="col-xs-4 colonne">
+	    <div class="row">
+	      <div class="dataLabel"> Identifiant : </div>
+	    </div>
+
+	    <div class="row">
+	      <div class="dataLabel"> Mot de passe : </div>
+	    </div>
+	  </div>
+
+	  <div class="col-xs-4 colonne">
+	    <div class="row">
+	      <div class="data"> unautremail@exemple.fr </div>
+	    </div>
+
+	    <div class="row">
+	      <div class="data"> *********** </div>
+	    </div>
+	  </div>
+
+	  <div class="col-xs-4 colonne">
+	    <div class="row">
+	      <a href="#" id="emailUpdate" style="color:grey;"> modifier </a>
+	    </div>
+
+	    <div class="row">
+	      <a href="#" id="passwordUpdate" style="color:grey;"> modifier </a>
+	    </div>
+	  </div>
+	</div>
+	
+	<div class="row" style="flex:1;">
+	  <div class="switch">
+	    <input type="checkbox">
+	    <span class="switchLabel">Afficher mot de passe.</span>
+	  </div>
+	</div>
+      </div>
+      
+
+    </div>
+  </div>
+
+@if (count($tasks) > 0)
+  @foreach ($tasks as $task)
+  <div class="row subcontainer">
+    <div class="blockHeader" style="text-align:center;">
+      <span class="applicationTitle"> Compte </span>
+      <a href="#" style="color:white;"> supprimer </a>
+    </div>
+    <div class="information">
+      <div class="col-sx-3" style="text-align:center;">
+        <img src="" label="Logo" class="logo"/>
+      </div>
+
+      <div class="col-xs-9" style="display:flex; flex-direction:column; justify-content:space-around;">
+
+	<div class="row" style="flex:3">
+
+	  <div class="col-xs-4 colonne">
+	    <div class="row">
+	      <div class="dataLabel"> Identifiant : </div>
+	    </div>
+
+	    <div class="row">
+	      <div class="dataLabel"> Mot de passe : </div>
+	    </div>
+	  </div>
+
+	  <div class="col-xs-4 colonne">
+	    <div class="row">
+	      <div class="data">  </div>
+	    </div>
+
+	    <div class="row">
+	      <div class="data"> {{ $task->name }} </div>
+	    </div>
+	  </div>
+
+	  <div class="col-xs-4 colonne">
+	    <div class="row">
+	      <a href="#" id="emailUpdate" style="color:grey;"> modifier </a>
+	    </div>
+
+	    <div class="row">
+	      <a href="#" id="passwordUpdate" style="color:grey;"> modifier </a>
+	    </div>
+	  </div>
+	</div>
+
+	<div class="row" style="flex:1;">
+	  <div class="switch">
+	    <input type="checkbox">
+	    <span class="switchLabel"> Afficher mot de passe</span>
+	  </div>
+	</div>
+      </div>
+   </div>
+</div>
+@endforeach
+@endif
+
+  <div class="row subcontainer">
+    <div class="blockHeader" style="text-align:center;">
+      <span class="applicationTitle"> Ajouter des identifiants </span>
+    </div>
+    <form action="{{ url('task')}}" method="POST">
+     {{ csrf_field() }}
+      <div class="row" style="flex:3;">
+	<div class="col-xs-3 colonne"> 
+	  <div class="row"><div class="dataLabel"> URL : </div><div class="column input" style="flex:2"><input type="text" name="url" id="urlInput" class="formInput" placeholder=""/></div></div>
+	  <div class="row"><div class="dataLabel"> Identifiant : </div><div class="column input" style="flex:2"><input type="text" name="name" id="idInput" class="formInput" placeholder="votremail@exemple.com"/></div></div>
+	  <div class="row"><div class="dataLabel"> Mot de passe : </div><div class="column input" style="flex:2"><input type="password" name="password" id="passwordInput" class="formInput" placeholder="Votre mot de passe"/></div></div>
+	</div>
+	
+	
+	<div class="col-xs-3 colonne">
+
+	</div>
+      </div>
+      
+      <div class="row" style="flex:1;">
+	<div class="buttonContainer">
+	  <input type="submit" name="monSubmit" id="submit" value="Ajouter" class="addButton" />
+	</div>
+      </div>
+    </form>
+  </div>
+</div>
 @endsection
