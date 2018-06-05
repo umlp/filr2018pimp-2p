ba6800c38dc2968322c2a8add7897dd7970725ea
pcadorel <36007955+pcadorel@users.noreply.github.com>
2018-06-05 16:23:45 +0200

    Update 2015_10_27_141258_create_tasks_table.php

--- database/migrations/2015_10_27_141258_create_tasks_table.php
+++ database/migrations/2015_10_27_141258_create_tasks_table.php
@@ -1,8 +1,6 @@
 <?php
-
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
-
 class CreateTasksTable extends Migration
 {
     /**
@@ -14,11 +12,10 @@ class CreateTasksTable extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');           
+            $table->string('name');
             $table->timestamps();
         });
     }
-
     /**
      * Reverse the migrations.
      *
